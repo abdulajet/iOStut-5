e55b6067447fbb99ded623d1cf7fb2dcff11f052
abdulajet <abdulhakimajet97@live.com>
2017-02-02 19:29:13 +0000

    first

--- weatherapi/ViewController.swift
+++ weatherapi/ViewController.swift
@@ -9,6 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var text: UITextField!
+    
+    @IBOutlet var label: UILabel!
+
+    var appKey = ""
+    
+    
+    @IBAction func button(_ sender: Any) {
+        
+        let url = URL(string:"http://api.openweathermap.org/data/2.5/weather?q=\(text.text!.replacingOccurrences(of: " ", with: "%20"))&appid=" + appKey)!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if  error != nil {
+                print(error!)
+            }else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        
+                        if let description = (((jsonResult as? NSDictionary)?["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String{
+                            
+                            
+                            DispatchQueue.main.sync(execute: {
+                                self.label.text = description
+                            })
+                            
+                            
+                        }
+                        
+                        
+                        
+                    } catch {
+                        
+                    }
+                    
+                }
+            }
+            
+        }
+        
+        task.resume()
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
